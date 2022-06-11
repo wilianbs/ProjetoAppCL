cd0814db6fcfbf834dff274b3a827c5120b3f347
Wilian <wilian.batista.santos@gmail.com>
2022-06-11 20:24:34 -0300

    add dados na no relatori

--- Produção/scriptProducao.js
+++ Produção/scriptProducao.js
@@ -1,8 +1,20 @@
 
 
+function selecionar(){
+
+   let btmProcesso = document.querySelector ("#condicao").value
+   let valorProcesso = btmProcesso
+   console.log (valorProcesso)
+ 
+}
+  
+ 
+
+
+
 const linhaDeDados= [];
 
-//inserindo dados no campo <span>
+//Criando campo <span> JS
 let d = document.querySelector('.dadosProd')
 let lid = document.createElement ("span")
 
@@ -13,17 +25,19 @@ function adicionarElementos() {
     linhaDeDados.push(Number(linhaDeDados.length+1))
     console.log(linhaDeDados)
     lid.appendChild(document.createTextNode(`${linhaDeDados}`))
+    lid.style.boxShadow= 'inset 0px -6px 5px -2px  rgba(1, 255, 1, 0.5)'
     d.appendChild(lid)
+
     lid.innerHTML= (`${linhaDeDados}`)
-    
     
 }
 function retirarElementos(){
 
     linhaDeDados.pop(Number(linhaDeDados.length-1))
-console.log(linhaDeDados)
+    console.log(linhaDeDados)
     lid.appendChild(document.createTextNode(`${linhaDeDados}`))
     d.appendChild(lid)
+
     lid.innerHTML= (`${linhaDeDados}`)
 
 }
